ee9a40aaf0ed9cc06e5c1d4bea160d4c451efcf5
Aya Mohamed <152428721+fou1100ouacus@users.noreply.github.com>
2024-09-03 08:24:43 +0300

    Add files via upload

---
+++ SQLQuery2.sql
@@ -0,0 +1,35 @@
+--use CarRental
+--ALTER TABLE Customers
+--     	FOREIGN KEY (PersonID) REFERENCES People(PersonID)
+
+
+--		ALTER TABLE Customers
+--     	drop  Name,ContactInformation; 
+
+--		CREATE TABLE Customerrs (
+--    CustomerID INT PRIMARY KEY,
+--	  PersonID INT NOT NULL,
+--	 FOREIGN KEY (PersonID) REFERENCES People(PersonID),
+--    DriverLicenseNumber NVARCHAR(20)
+--)
+
+--alter table RentalBooking
+--add
+--  CustomerID INT FOREIGN KEY REFERENCES Customerrs(CustomerID)
+
+------------------
+		select * from people
+		SELECT * FROM users
+select * from Customerrs
+select * from RentalBooking
+select * from Vehicles
+
+
+
+
+
+
+
+
+
+
